bdb4d2eb40a2b1ec5c881510224a7c7fd51cd1c7
mdmonauwarulislam <mdmonauwarulislam@gmail.com>
2024-12-26 23:46:14 +0530

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { RepositoryList } from './components/RepositoryList';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import AICodeReview from './components/AICodeReview';
 import CloudSecurity from './components/CloudSecurity';
 import HowToUse from './components/HowToUse';
@@ -11,27 +11,34 @@ import Login from './components/Login';
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const location = useLocation();
 
   return (
     <>
       {
-        location.pathname === '/' ? <Login /> : (
-          <div className="min-h-screen bg-white">
-            <div className="flex">
-              <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
-              <Header onMenuToggle={() => setIsSidebarOpen(true)} />
-              <main className="flex-1 lg:p-6 max-h-screen overflow-y-auto">
-                <Routes>
-                  <Route path="/dashboard" element={<RepositoryList />} />
-                  <Route path="/dashboard/ai-code-review" element={<AICodeReview />} />
-                  <Route path="/dashboard/cloud-security" element={<CloudSecurity />} />
-                  <Route path="/dashboard/how-to-use" element={<HowToUse />} />
-                  <Route path="/dashboard/settings" element={<Settings />} />
-                </Routes>
-              </main>
+        location.pathname === '/' ? (
+          <Login setIsLoggedIn={setIsLoggedIn} /> 
+        ) : (
+          isLoggedIn ? (
+            <div className="min-h-screen bg-white">
+              <div className="flex">
+                <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+                <Header onMenuToggle={() => setIsSidebarOpen(true)} />
+                <main className="flex-1 lg:p-6 max-h-screen overflow-y-auto">
+                  <Routes>
+                    <Route path="/dashboard" element={<RepositoryList />} />
+                    <Route path="/dashboard/ai-code-review" element={<AICodeReview />} />
+                    <Route path="/dashboard/cloud-security" element={<CloudSecurity />} />
+                    <Route path="/dashboard/how-to-use" element={<HowToUse />} />
+                    <Route path="/dashboard/settings" element={<Settings />} />
+                  </Routes>
+                </main>
+              </div>
             </div>
-          </div>
+          ) : (
+            <Navigate to="/" />
+          )
         )
       }
     </>
